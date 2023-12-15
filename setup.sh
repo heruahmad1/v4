eeeec2a5f1b40596f92c1f8157855a4fdd002c90
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-15 17:18:19 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -187,8 +187,38 @@ clear
 #Instal udp
 echo -e "install menu server"
 sleep 2
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
 wget https://raw.githubusercontent.com/heruahmad1/v4/main/xray/up.sh && chmod +x up.sh && ./up.sh
+}
+netfilter-persistent
 clear
+fun_bar 'res1'
 
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
