d7669bf5224f71823a5f0a6028c5e9ea505006f6
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-16 12:16:06 +0800

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -244,11 +244,6 @@ wget -q -O /root/cf.sh "https://raw.githubusercontent.com/sasak3/v4/main/slowdns
 chmod +x /root/cf.sh
 ./cf.sh
 clear
-else
-print_install "Random Subdomain/Domain is Used"
-wget -q -O /root/cf.sh "https://raw.githubusercontent.com/sasak3/v4/main/slowdns/cf.sh"
-rm -f /root/cf.sh
-clear
     elif test $dom -eq 2; then
     read -rp "Enter Your Domain : " domen 
     echo $domen > /root/domain
@@ -401,61 +396,17 @@ else
 gg="AM"
 fi
 curl -sS ifconfig.me > /etc/myipvps
-
+echo "------------------------------------------------------------"
 echo " "
 echo "=====================-[ AutoScript HR-Project ]-===================="
 echo ""
 echo "------------------------------------------------------------"
 echo ""
-echo ""
-echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - SSH Websocket           : 80" | tee -a log-install.txt
-echo "   - SSH SSL Websocket       : 443" | tee -a log-install.txt
-echo "   - SSH NON-SSL Websocket   : 80, 8880" | tee -a log-install.txt
-echo "   - SLOWDNS                 : 5300 [OFF]" | tee -a log-install.txt
-echo "   - Stunnel4                : 447, 777" | tee -a log-install.txt
-echo "   - Dropbear                : 109, 143" | tee -a log-install.txt
-echo "   - Badvpn                  : 7100-7900" | tee -a log-install.txt
-echo "   - Nginx                   : 81" | tee -a log-install.txt
-echo "   - XRAY  Vmess TLS         : 443" | tee -a log-install.txt
-echo "   - XRAY  Vmess None TLS    : 80" | tee -a log-install.txt
-echo "   - XRAY  Vless TLS         : 443" | tee -a log-install.txt
-echo "   - XRAY  Vless None TLS    : 80" | tee -a log-install.txt
-echo "   - Trojan GRPC             : 443" | tee -a log-install.txt
-echo "   - Trojan WS               : 443" | tee -a log-install.txt
-echo "   - Sodosok WS/GRPC         : 443" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On           : $aureb:00 $gg GMT +7" | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - AutoKill Multi Login User" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Fully automatic script" | tee -a log-install.txt
-echo "   - VPS settings" | tee -a log-install.txt
-echo "   - Admin Control" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> WhatsApp : ------------------ (Text Only)"  | tee -a log-install.txt
-echo "   >>> Telegram : t.me/heruahmad"  | tee -a log-install.txt
-echo ""
-echo "------------------------------------------------------------"
-echo ""
-echo "===============-[ Script HR-PROJECT  ]-==============="
-echo -e ""
-echo ""
 echo "" | tee -a log-install.txt
 rm /root/cf.sh >/dev/null 2>&1
 rm /root/instal.sh >/dev/null 2>&1
 rm /root/insshws.sh 
-rm /root/update.sh
+rm /root/up.sh
 rm /root/nontls.sh
 rm /root/slow.sh
 secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
